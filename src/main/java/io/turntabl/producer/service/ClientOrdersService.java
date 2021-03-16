6d9ae18900fedb91e7462415a0743f620ddc28af
Smart <kwesi.smart@turntabl.io>
2021-03-16 14:51:29 +0000

    Get client balance for OVS

--- src/main/java/io/turntabl/producer/service/ClientOrdersService.java
+++ src/main/java/io/turntabl/producer/service/ClientOrdersService.java
@@ -2,16 +2,31 @@ package io.turntabl.producer.service;
 
 import io.turntabl.producer.clientorders.OrderRequest;
 import io.turntabl.producer.clientorders.OrderResponse;
+
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ClientOrdersService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    Environment env;
+
     public OrderResponse checkOrderValidity(OrderRequest request){
         OrderResponse response = new OrderResponse();
 
-        if(request.getSide().equals("BUY")){
-            if((request.getPrice()* request.getQuantity())>=200){
+        Double clientBalance = restTemplate.getForObject(env.getProperty("clientservice")
+            .concat("/portfolio/client-balance/").concat(request.getPortfolioId()), Double.class);
+        
+        System.out.println("Client balance: " + clientBalance);
+
+        if(request.getSide().equals("BUY")) {
+            if((request.getPrice() * request.getQuantity()) >= 200){
                 response.setIsOrderValid(true);
                 response.setMessage("Client order is valid");
             }else{
