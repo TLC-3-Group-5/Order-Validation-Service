0085072af54fefcd915502675fe1b1ccf70bf786
Smart <kwesi.smart@turntabl.io>
2021-03-22 15:17:10 +0000

    Change hard-coded 'localhost' to use redis url from env

--- src/main/java/io/turntabl/producer/service/ClientOrdersService.java
+++ src/main/java/io/turntabl/producer/service/ClientOrdersService.java
@@ -26,47 +26,48 @@ public class ClientOrdersService {
     private final MarketDataService marketDataService;
 
     @Autowired
-    private  final PortfolioService portfolioService;
+    private final PortfolioService portfolioService;
 
     @Autowired
-    private  final OrderService orderService;
+    private final OrderService orderService;
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     private RestTemplate restTemplate;
 
-
     @Autowired
     private Environment env;
 
     @Autowired
-    public ClientOrdersService(MarketDataService marketDataService, PortfolioService portfolioService, OrderService orderService) {
+    public ClientOrdersService(MarketDataService marketDataService, PortfolioService portfolioService,
+            OrderService orderService) {
         this.marketDataService = marketDataService;
         this.portfolioService = portfolioService;
         this.orderService = orderService;
     }
 
-    public OrderResponse checkOrderValidity(OrderRequest request){
+    public OrderResponse checkOrderValidity(OrderRequest request) {
         OrderResponse response = new OrderResponse();
 
         Double clientBalance = restTemplate.getForObject(env.getProperty("app.client_connectivity_service_url")
-            .concat("/portfolio/client-balance/").concat(String.valueOf((request.getPortfolioId()))), Double.class);
+                .concat("/portfolio/client-balance/").concat(String.valueOf((request.getPortfolioId()))), Double.class);
 
-        double balance = clientBalance != null ? clientBalance: 0;
+        double balance = clientBalance != null ? clientBalance : 0;
 
         OwnedStockList stockList = restTemplate.getForObject(env.getProperty("app.client_connectivity_service_url")
-                .concat("/portfolio/client-stocks/").concat(String.valueOf((request.getPortfolioId()))), OwnedStockList.class);
+                .concat("/portfolio/client-stocks/").concat(String.valueOf((request.getPortfolioId()))),
+                OwnedStockList.class);
 
         MarketData marketData = marketDataService.getMarketDataByTicker(request.getProduct());
 
         System.out.println(marketData);
-        //TODO Check BidPrice in the validation
-        if(request.getSide().equals("BUY")) {
-            if(balance!= 0 && (request.getPrice() * request.getQuantity()) <=balance){
-                if(marketData!=null){
-                    if(marketData.getBuyLimit()>0){
-                        if(request.getQuantity()<marketData.getBuyLimit()){
+        // TODO Check BidPrice in the validation
+        if (request.getSide().equals("BUY")) {
+            if (balance != 0 && (request.getPrice() * request.getQuantity()) <= balance) {
+                if (marketData != null) {
+                    if (marketData.getBuyLimit() > 0) {
+                        if (request.getQuantity() < marketData.getBuyLimit()) {
                             Orders orders = new Orders();
                             orders.setStatus("OPEN");
                             orders.setSide(request.getSide());
@@ -77,42 +78,42 @@ public class ClientOrdersService {
                             orders.setPortfolio(portfolioService.getPortfolio((long) request.getPortfolioId()));
                             orderService.createOrders(orders);
 
-                            try{
-                                Jedis client = new Jedis("localhost", 6379);
+                            try {
+                                Jedis client = new Jedis(env.getProperty("app.SPRING_REDIS_URL"),
+                                        Integer.parseInt(env.getProperty("app.SPRING_REDIS_PORT")));
                                 client.publish("orders", objectMapper.writeValueAsString(orders));
-                            }catch(Exception e){
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
                             response.setIsOrderValid(true);
                             response.setMessage("Client order is valid");
-                        }else{
+                        } else {
                             response.setIsOrderValid(false);
                             response.setMessage("Your requested quantity is greater than the buy limit");
                         }
-                    }else{
+                    } else {
                         response.setIsOrderValid(false);
                         response.setMessage("Product is unavailable no limit");
                     }
-                }else{
+                } else {
                     response.setIsOrderValid(false);
                     response.setMessage("Product is unavailable");
                 }
-            }else{
+            } else {
                 response.setIsOrderValid(false);
                 response.setMessage("Client balance insufficient for the order");
             }
         }
 
-        if(request.getSide().equals("SELL")){
+        if (request.getSide().equals("SELL")) {
             assert stockList != null;
             OwnedStock stock = stockList.getOwnedStockList().stream()
-                    .filter(ownedStock->ownedStock.getTicker().equals(request.getProduct()))
-                    .findFirst().orElse(null);
-            if(stock!=null){
-                if(marketData.getSellLimit()>0){
-                    if(request.getQuantity()<marketData.getSellLimit()){
+                    .filter(ownedStock -> ownedStock.getTicker().equals(request.getProduct())).findFirst().orElse(null);
+            if (stock != null) {
+                if (marketData.getSellLimit() > 0) {
+                    if (request.getQuantity() < marketData.getSellLimit()) {
 
-                        //TODO Push order to Trade Engine via Content Pub/Sub
+                        // TODO Push order to Trade Engine via Content Pub/Sub
                         Orders orders = new Orders();
                         orders.setStatus("SELL");
                         orders.setSide(request.getSide());
@@ -123,23 +124,24 @@ public class ClientOrdersService {
                         orders.setPortfolio(portfolioService.getPortfolio((long) request.getPortfolioId()));
                         orderService.createOrders(orders);
 
-                        try{
-                            Jedis client = new Jedis("localhost", 6379);
+                        try {
+                            Jedis client = new Jedis(env.getProperty("app.SPRING_REDIS_URL"),
+                                Integer.parseInt(env.getProperty("app.SPRING_REDIS_PORT")));
                             client.publish("orders", objectMapper.writeValueAsString(orders));
-                        }catch(Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                         response.setIsOrderValid(true);
                         response.setMessage("Client order is valid");
-                    }else{
+                    } else {
                         response.setIsOrderValid(false);
-                        response.setMessage("You cannot sell more than " + request.getQuantity() );
+                        response.setMessage("You cannot sell more than " + request.getQuantity());
                     }
-                }else{
+                } else {
                     response.setIsOrderValid(false);
                     response.setMessage("You cannot sell here");
                 }
-            }else{
+            } else {
                 response.setIsOrderValid(false);
                 response.setMessage("You don't have this product on your portfolio");
             }
