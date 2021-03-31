e251a4edadb5d969df002e70edfdbd3ce19769f7
kyfs-tt <78965733+kyfs-tt@users.noreply.github.com>
2021-03-31 13:14:23 +0000

    Revert "No exchange 2"

--- src/main/java/io/turntabl/producer/resources/controller/OrderController.java
+++ src/main/java/io/turntabl/producer/resources/controller/OrderController.java
@@ -1,7 +1,6 @@
 package io.turntabl.producer.resources.controller;
 
 import io.turntabl.producer.resources.model.Trade;
-import io.turntabl.producer.resources.model.TradeList;
 import io.turntabl.producer.resources.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,20 +18,12 @@ public class OrderController {
     }
 
     @GetMapping(path="order-trades/{orderId}")
-    public TradeList getOrderTrade(@PathVariable("orderId") String orderId){
-        List<Trade> trades = orderService.getOrderTrades(orderId);
-        TradeList tradeList = new TradeList();
-        tradeList.setTradeList(trades);
-        return tradeList;
+    public List<Trade> getOrderTrade(@PathVariable("orderId") String orderId){
+        return orderService.getOrderTrades(orderId);
     }
 
     @PutMapping(path="update-order-status/{orderId}")
     public void updateOrderStatus(@PathVariable("orderId") Long orderId, @RequestBody String status){
         orderService.updateOrderStatus(orderId,status);
-    }
-
-    @PutMapping(path="update-balance-of-cancelled-order/{orderId}")
-    public void updateBalanceOfCancelledOrder(@PathVariable("orderId") String orderId){
-        orderService.updateBalanceOfCancelledOrder(orderId);
     }
 }
--- src/main/java/io/turntabl/producer/resources/service/OrderService.java
+++ src/main/java/io/turntabl/producer/resources/service/OrderService.java
@@ -4,26 +4,14 @@ import io.turntabl.producer.resources.model.Orders;
 import io.turntabl.producer.resources.model.Trade;
 import io.turntabl.producer.resources.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
-
-    @Value("${app.client_connectivity_service_url}")
-    private String clientUrl;
-
-    @Autowired
-    RestTemplate restTemplate;
 
     @Autowired
     public OrderService(OrderRepository orderRepository){
@@ -50,23 +38,7 @@ public class OrderService {
         Orders order = orderRepository.findById(id).orElse(null);
         if(order!=null){
             order.setStatus(status);
-            order.setUpdatedAt(LocalDateTime.now());
             orderRepository.save(order);
-        }
-    }
-
-    // Update Client Balance of Cancelled Order
-    public void updateBalanceOfCancelledOrder(String orderId){
-        Orders order = orderRepository.findById(Long.valueOf(orderId)).orElse(null);
-
-        if(order != null){
-            if(order.getStatus().equals("OPEN")){
-                Map<String, Long> variables = new HashMap<>();
-                variables.put("portfolioId", order.getPortfolio().getId());
-                restTemplate.put(
-                        clientUrl.concat("/portfolio/update-balance/{portfolioId}"),
-                        (order.getPrice()*order.getQuantity()), variables);
-            }
         }
     }
 }
